19559149b5452dd13d68c312eddf5957c10d2cf8
Kam Woods <kamwoods@gmail.com>
2019-06-30 01:16:39 -0400

    Added basic install wrapper for salt and distro setup

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#--- FUNCTION ---------------------------------------------------------
+# NAME: echoinfo
+# DESCRIPTION: Echo information to stdout.
+#----------------------------------------------------------------------
+echoinfo() {
+    printf "%s * STATUS%s: %s\n" "${GC}" "${EC}" "$@";
+}
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run with sudo"
+   exit 1
+fi
+
+echoinfo "Installing git..."
+apt-get install -y git
+
+echoinfo "Installing salt..."
+apt-get install -y salt-minion
+service salt-minion stop
+
+echoinfo "Getting latest BitCurator release files..."
+git clone https://github.com/bitcurator/bitcurator-distro-salt /srv/salt
+
+echoinfo "BitCurator provides desktop customizations (desktop folder icons, an appindicator to disable USB write access, and a custom background) for an existing user. Please enter the username now."
+read -p "Username: " USERID
+#echo $USERID
+
+echoinfo "Installing BitCurator tools and scripts..."
+salt-call --local state.sls bitcurator.primary pillar='{"bitcurator_version": "dev", "bitcurator_user": $USERID}'
