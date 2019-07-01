b95375abe7440e02908bab36ffa0be6e63e7821a
Kam Woods <kamwoods@gmail.com>
2019-07-01 01:02:06 -0400

    Minor install.sh update

--- install.sh
+++ install.sh
@@ -13,21 +13,29 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-echoinfo "Installing git..."
+echoinfo " ***********************************************************"
+echoinfo "                                                            "
+echoinfo " BitCurator provides desktop customizations (desktop folder "
+echoinfo " icons, an appindicator to disable USB write access, and a  "
+echoinfo " custom background) for an existing user. Please enter the  "
+echoinfo " username now.                                              "
+echoinfo "                                                            "
+echoinfo " ***********************************************************"
+echoinfo "                                                            "
+
+read -p " Enter username: " USERID
+
+echoinfo " Installing git..."
 apt-get install -y git
 
-echoinfo "Installing salt..."
+echoinfo " Installing salt..."
 apt-get install -y salt-minion
 service salt-minion stop
 
-echoinfo "Getting latest BitCurator release files..."
+echoinfo " Getting latest BitCurator release files..."
 git clone https://github.com/bitcurator/bitcurator-distro-salt /srv/salt
 cd /srv/salt
 git checkout ENHANCEMENT-install
 
-echoinfo "BitCurator provides desktop customizations (desktop folder icons, an appindicator to disable USB write access, and a custom background) for an existing user. Please enter the username now."
-read -p "Username: " USERID
-#echo $USERID
-
-echoinfo "Installing BitCurator tools and scripts..."
+echoinfo " Installing BitCurator tools and scripts..."
 salt-call -l info --local state.sls bitcurator.primary pillar='{"bitcurator_version": "dev", "bitcurator_user": '$USERID'}'
